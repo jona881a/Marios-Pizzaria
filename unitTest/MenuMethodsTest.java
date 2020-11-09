0bd314838550dbd215744db8ff493411e88c3777
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-11-09 13:13:16 +0100

    2 kopper kaffe går i enderen

---
+++ unitTest/MenuMethodsTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuMethodsTest {
+
+    MenuMethods menuMethods = new MenuMethods();
+
+    @Test
+    void showMenu() {
+        menuMethods.showMenu();
+    }
+
+    @Test
+    void addOrders() {
+    }
+
+    @Test
+    void removeOrder() {
+    }
+
+    @Test
+    void getOrderHistory() {
+    }
+}
